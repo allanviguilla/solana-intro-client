25279e226b4c7aaf1e9abcca44a8a43020b5e5e6
allanviguilla <viguilla.allan@gmail.com>
2023-04-18 00:18:46 -0700

    Call an on-chain program

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import * as Web3 from "@solana/web3.js";
 import * as fs from "fs";
 import dotenv from "dotenv";
 
+const PROGRAM_ID = new Web3.PublicKey(
+  "ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa"
+);
+const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey(
+  "Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod"
+);
+
 async function initializeKeypair(
   connection: Web3.Connection
 ): Promise<Web3.Keypair> {
@@ -18,6 +25,8 @@ async function initializeKeypair(
   const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[];
   const secretKey = Uint8Array.from(secret);
   const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+
+  await airdropSolIfNeeded(keypairFromSecret, connection);
 
   return keypairFromSecret;
 }
@@ -51,6 +60,38 @@ async function airdropSolIfNeeded(
   }
 }
 
+async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair) {
+  const transaction = new Web3.Transaction();
+  const instruction = new Web3.TransactionInstruction({
+    // Instructions need 3 things
+
+    // 1. The public keys of all the accounts the instruction will read/write
+    keys: [
+      {
+        pubkey: PROGRAM_DATA_PUBLIC_KEY,
+        isSigner: false,
+        isWritable: true,
+      },
+    ],
+
+    // 2. The ID of the program this instruction will be sent to
+    programId: PROGRAM_ID,
+
+    // 3. Data - in this case, there's none!
+  });
+
+  transaction.add(instruction);
+  const transactionSignature = await Web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [payer]
+  );
+
+  console.log(
+    `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  );
+}
+
 dotenv.config();
 
 async function main() {
@@ -58,6 +99,8 @@ async function main() {
   const signer = await initializeKeypair(connection);
 
   console.log("Public key: ", signer.publicKey.toBase58());
+
+  await pingProgram(connection, signer);
 }
 
 main()
